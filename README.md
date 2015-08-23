1d399dfdda83138eb489d946da5856c84dc898bb
liquidz <liquidz.uo@gmail.com>
2015-08-23 17:53:14 +0900

    add license, readme and travis.yml

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 Pipette
 =======
 
-** TODO: Add description **
+## Installation
+
+* mix.exs
+```elixir
+def deps do
+  [{:pipette, "~> 0.0.1"}]
+end
+```
+
+## Usage
+
+* build
+```elixir
+iex> File.read!("hello.txt")
+"hello, <%= msg %>\n"
+iex> Pipette.build(%{template: "hello.txt", destination: "world.txt", msg: "world"})
+:ok
+iex> File.read!("world.txt")
+"hello, world"
+```
+* pre hook
+```elixir
+iex> data = %{template: "hello.txt", destination: "world.txt", msg: "world"}
+iex> Pipette.build(data, pre: &Dict.put(&1, :msg, "pipette"))
+:ok
+iex> File.read!("world.txt")
+"hello, pipette"
+```
+* post hook
+```elixir
+iex> data = %{template: "hello.txt", destination: "world.txt", msg: "world"}
+iex> Pipette.build(data, post: &("[#{&1}]")
+:ok
+iex> File.read!("world.txt")
+"[hello, world]"
+```
+
+## License
+
+Copyright (C) 2015 Masashi Iizuka([@uochan](http://twitter.com/uochan))
+
+Distributed under the MIT License.
--- mix.exs
+++ mix.exs
@@ -1,38 +1,42 @@
 defmodule Pipette.Mixfile do
   use Mix.Project
 
+  @url "https://github.com/liquidz/pipette"
+  @description """
+  new_data = pipette(data, template)
+  """
+
   def project do
-    [app:          :pipette,
-     version:      "0.0.1",
-     elixir:       "~> 1.0",
-     name:         "pipette",
-     source_url:   "https://github.com/liquidz/pipette",
-     homepage_url: "https://github.com/liquidz/pipette",
-     deps:         deps]
+    [
+      app:          :pipette,
+      version:      "0.0.1",
+      elixir:       "~> 1.0",
+      name:         "R1",
+      source_url:   @url,
+      description:  @description,
+      package:      package,
+      deps:         deps
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:mock,    "~> 0.1.1"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc,  "~> 0.8", only: :dev},
       {:junit_formatter, "~> 0.0.2", only: :test},
+    ]
+  end
+
+  defp package do
+    [
+      contributors: ["Masashi Iizuka"],
+      licenses:     ["MIT"],
+      links:        %{"Github" => @url}
     ]
   end
 end
